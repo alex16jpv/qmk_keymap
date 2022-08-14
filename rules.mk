28b48da795e02d1daa5b4797d95c3b61c0b068d2
Alexander Valencia <aperez@hellobuild.co>
2022-08-14 01:42:11 -0500

    Feat: Add rgb and macros

--- rules.mk
+++ rules.mk
@@ -1,1 +1,7 @@
 SRC += source.c
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+DYNAMIC_MACRO_ENABLE = yes
